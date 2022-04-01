aa83af03d82d625ea41bc04ef13f3dfaa64ce67c
kimsolo <rop477@163.com>
2022-04-01 16:57:39 +0800

    fix the plot bugs

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ from spikingjelly.datasets.n_mnist import NMNIST
 from torch.utils.data import Dataset as BaseDataset
 from torch.utils.data import DataLoader
 import os
-
+import sys
 
 parser = argparse.ArgumentParser(description='spikingjelly LIF MNIST Training')
 
 parser.add_argument('--device', default='cuda:0', help='运行的设备，例如“cpu”或“cuda:0”\n Device, e.g., "cpu" or "cuda:0"')
 
 parser.add_argument('--dataset-dir', default='./', help='保存MNIST数据集的位置，例如“./”\n Root directory for saving MNIST dataset, e.g., "./"')
-parser.add_argument('--log-dir', default='./', help='保存tensorboard日志文件的位置，例如“./”\n Root directory for saving tensorboard logs, e.g., "./"')
+parser.add_argument('--log-dir', default='./result', help='保存tensorboard日志文件的位置，例如“./”\n Root directory for saving tensorboard logs, e.g., "./"')
 parser.add_argument('--model-output-dir', default='./', help='模型保存路径，例如“./”\n Model directory for saving, e.g., "./"')
 
-parser.add_argument('-b', '--batch-size', default=128, type=int, help='Batch 大小，例如“64”\n Batch size, e.g., "64"')
+parser.add_argument('-b', '--batch-size', default=160, type=int, help='Batch 大小，例如“64”\n Batch size, e.g., "64"')
 parser.add_argument('-T', '--timesteps', default=100, type=int, dest='T', help='仿真时长，例如“100”\n Simulating timesteps, e.g., "100"')
 parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, metavar='LR', help='学习率，例如“1e-3”\n Learning rate, e.g., "1e-3": ', dest='lr')
 parser.add_argument('--tau', default=2.0, type=float, help='LIF神经元的时间常数tau，例如“100.0”\n Membrane time constant, tau, for LIF neurons, e.g., "100.0"')
@@ -79,6 +79,7 @@ class Dataset(BaseDataset):
     def __len__(self):
         return len(self.input)
 
+
 class STN3d(nn.Module):
     def __init__(self):
         super(STN3d, self).__init__()
@@ -111,7 +112,7 @@ class STN3d(nn.Module):
         x = F.relu(self.bn4(self.fc1(x)))
         x = self.dropout1(x)
         x = F.relu(self.bn5(self.fc2(x)))
-        x = self.dropout2(x)
+        
         x = self.fc3(x)
 
         # iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
@@ -120,7 +121,19 @@ class STN3d(nn.Module):
         # x = x + iden
         # x = x.view(-1, 3, 3)
         return x
+
     
+def format_logs(logs):
+    str_logs = ['{} - {:.4}'.format(k, v) for k, v in logs.items()]
+    s = ', '.join(str_logs)
+    return s
+
+
+def event_aug(input_array):
+    choose = np.random.randint(1024, 2048)
+    input_array[:,:,1024 : choose] = 0
+    return input_array
+
 
 def main():
     '''
@@ -180,70 +193,66 @@ def main():
 
     test_accs = []
     train_accs = []
-
+    loss_ce = torch.nn.CrossEntropyLoss()
     for epoch in range(train_epoch):
         print("Epoch {}:".format(epoch))
         print("Training...")
         train_correct_sum = 0
         train_sum = 0
         net.train()
-        for img, label in tqdm(train_loader):
-            img = img.to(device)
-            label = label.to(device)
-            label_one_hot = F.one_hot(label, 10).float()
-
-            optimizer.zero_grad()
-
-            # 运行T个时长，out_spikes_counter是shape=[batch_size, 10]的tensor
-            # 记录整个仿真时长内，输出层的10个神经元的脉冲发放次数
-            # input_pic = torch.Tensor(img.shape[0], T, img.shape[1], img.shape[2])
-            # for t in range(T):
-            #     input_pic[:,t,:,:] = encoder(img)
-            # for t in range(T):
-            #     if t == 0:
-            #         out_spikes_counter = net(encoder(img).float())
-            #     else:
-            #         out_spikes_counter += net(encoder(img).float())
-            # # out_spikes_counter = net(
-            # # out_spikes_counter / T 得到输出层10个神经元在仿真时长内的脉冲发放频率
-            # out_spikes_counter_frequency = out_spikes_counter / T
-            output = net(img)
-            # 损失函数为输出层神经元的脉冲发放频率，与真实类别的MSE
-            # 这样的损失函数会使，当类别i输入时，输出层中第i个神经元的脉冲发放频率趋近1，而其他神经元的脉冲发放频率趋近0
-            loss = F.mse_loss(output, label_one_hot)
-            loss.backward()
-            optimizer.step()
-            # 优化一次参数后，需要重置网络的状态，因为SNN的神经元是有“记忆”的
-            functional.reset_net(net)
-
-            # 正确率的计算方法如下。认为输出层中脉冲发放频率最大的神经元的下标i是分类结果
-            train_correct_sum += (output.max(1)[1] == label.to(device)).float().sum().item()
-            train_sum += label.numel()
-
-            train_batch_accuracy = (output.max(1)[1] == label.to(device)).float().mean().item()
-            writer.add_scalar('train_batch_accuracy', train_batch_accuracy, train_times)
-            train_accs.append(train_batch_accuracy)
-
-            train_times += 1
+        with tqdm(train_loader, file=sys.stdout) as iterator:
+            for img, label in iterator:
+                img = img.to(device)
+                label = label.to(device)
+                img = event_aug(img)
+                # label_one_hot = F.one_hot(label, 10).float()
+                # label_one_hot = F.one_hot(label, 10)
+                # img = F.dropout(img, p = 0.2) * 0.8
+                optimizer.zero_grad()
+    
+    
+                output = net(img)
+    
+                # loss = F.mse_loss(output, label_one_hot)
+                loss = loss_ce(output, label)
+                loss.backward()
+                optimizer.step()
+                
+    
+                # 正确率的计算方法如下。认为输出层中脉冲发放频率最大的神经元的下标i是分类结果
+                train_correct_sum += (output.max(1)[1] == label.to(device)).float().sum().item()
+                train_sum += label.numel()
+    
+                train_batch_accuracy = (output.max(1)[1] == label.to(device)).float().mean().item()
+                writer.add_scalar('train_batch_accuracy', train_batch_accuracy, train_times)
+                train_accs.append(train_batch_accuracy)
+    
+                train_times += 1
+                loss_logs = {'ce_loss': loss}
+                iterator.set_postfix_str(format_logs(loss_logs))
         train_accuracy = train_correct_sum / train_sum
 
         print("Testing...")
         net.eval()
-        with torch.no_grad():
-            # 每遍历一次全部数据集，就在测试集上测试一次
-            test_correct_sum = 0
-            test_sum = 0
-            for img, label in tqdm(valid_loader):
-                img = img.to(device)
-                # for t in range(T):
-                #     if t == 0:
-                #         out_spikes_counter = net(encoder(img).float())
-                #     else:
-                #         out_spikes_counter += net(encoder(img).float())
-                output = net(img)
-                test_correct_sum += (output.max(1)[1] == label.to(device)).float().sum().item()
-                test_sum += label.numel()
-                functional.reset_net(net)
+        with tqdm(valid_loader, file=sys.stdout) as iterator:
+            # for img, label in iterator:
+            with torch.no_grad():
+                
+                test_correct_sum = 0
+                test_sum = 0
+                for img, label in iterator:
+                    img = img.to(device)
+                    label = label.to(device)
+                    label_one_hot = F.one_hot(label, 10)
+                  
+                    output = net(img)
+                    loss = loss_ce(output, label)
+                    loss_logs = {'ce_loss': loss}
+                    
+                    iterator.set_postfix_str(format_logs(loss_logs))
+                    test_correct_sum += (output.max(1)[1] == label.to(device)).float().sum().item()
+                    test_sum += label.numel()
+                # functional.reset_net(net)
             test_accuracy = test_correct_sum / test_sum
             writer.add_scalar('test_accuracy', test_accuracy, epoch)
             test_accs.append(test_accuracy)
@@ -252,32 +261,32 @@ def main():
         print()
     
     # 保存模型
-    torch.save(net, model_output_dir + "/lif_snn_mnist.ckpt")
-    # 读取模型
-    # net = torch.load(model_output_dir + "/lif_snn_mnist.ckpt")
+    torch.save(net, model_output_dir + "/n_mnist.pt")
+    # # 读取模型
+    # # net = torch.load(model_output_dir + "/lif_snn_mnist.ckpt")
 
-    # 保存绘图用数据
-    net.eval()
-    with torch.no_grad():
-        img, label = valid_dataset[0]        
-        img = img.to(device)
-        for t in range(T):
-            if t == 0:
-                out_spikes_counter = net(encoder(img).float())
-            else:
-                out_spikes_counter += net(encoder(img).float())
-        out_spikes_counter_frequency = (out_spikes_counter / T).cpu().numpy()
-        print(f'Firing rate: {out_spikes_counter_frequency}')
-        output_layer = net[-1] # 输出层
-        v_t_array = output_layer.v.cpu().numpy().squeeze().T  # v_t_array[i][j]表示神经元i在j时刻的电压值
-        np.save("v_t_array.npy",v_t_array)
-        s_t_array = output_layer.spike.cpu().numpy().squeeze().T  # s_t_array[i][j]表示神经元i在j时刻释放的脉冲，为0或1
-        np.save("s_t_array.npy",s_t_array)
+    # # 保存绘图用数据
+    # net.eval()
+    # # with torch.no_grad():
+    # #     img, label = valid_dataset[0]        
+    # #     img = img.to(device)
+    # #     for t in range(T):
+    # #         if t == 0:
+    # #             out_spikes_counter = net(encoder(img).float())
+    # #         else:
+    # #             out_spikes_counter += net(encoder(img).float())
+    # #     out_spikes_counter_frequency = (out_spikes_counter / T).cpu().numpy()
+    # #     print(f'Firing rate: {out_spikes_counter_frequency}')
+    # #     output_layer = net[-1] # 输出层
+    # #     v_t_array = output_layer.v.cpu().numpy().squeeze().T  # v_t_array[i][j]表示神经元i在j时刻的电压值
+    # #     np.save("v_t_array.npy",v_t_array)
+    # #     s_t_array = output_layer.spike.cpu().numpy().squeeze().T  # s_t_array[i][j]表示神经元i在j时刻释放的脉冲，为0或1
+    # #     np.save("s_t_array.npy",s_t_array)
 
-    train_accs = np.array(train_accs)
-    np.save('train_accs.npy', train_accs)
-    test_accs = np.array(test_accs)
-    np.save('test_accs.npy', test_accs)
+    # train_accs = np.array(train_accs)
+    # np.save('train_accs.npy', train_accs)
+    # test_accs = np.array(test_accs)
+    # np.save('test_accs.npy', test_accs)
 
 
 if __name__ == '__main__':
